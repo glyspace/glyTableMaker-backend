2674aeb4faf5af84728e5acb86b555ef9c0c91a9
Sena Arpinar <sena@admins-mbp-2.lan>
2024-10-02 15:22:38 -0400

    modified site glycans to have type

--- src/main/java/org/glygen/tablemaker/persistence/protein/Site.java
+++ src/main/java/org/glygen/tablemaker/persistence/protein/Site.java
@@ -1,23 +1,17 @@
 package org.glygen.tablemaker.persistence.protein;
 
-import org.glygen.tablemaker.persistence.glycan.Glycan;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Site {
 	
 	Long siteId;
 	String position;
-	java.util.Collection<Glycan> glycans;
+	java.util.Collection<GlycanInSite> glycans;
 	
 	@Id
 	@GeneratedValue
@@ -36,17 +30,12 @@ public class Site {
 		this.position = position;
 	}
 	
-	@ManyToMany(fetch=FetchType.EAGER)
-	@JoinTable(
-		    name="site_glycans",
-		    joinColumns=@JoinColumn(name="siteid"),
-		    inverseJoinColumns=@JoinColumn(name="glycanid")
-		)
-	public java.util.Collection<Glycan> getGlycans() {
+	@OneToMany(mappedBy = "site")
+	public java.util.Collection<GlycanInSite> getGlycans() {
 		return glycans;
 	}
 	
-	public void setGlycans(java.util.Collection<Glycan> glycans) {
+	public void setGlycans(java.util.Collection<GlycanInSite> glycans) {
 		this.glycans = glycans;
 	}
 
