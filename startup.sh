57d0f7be9354786e58046f80d9cdf485a0f3195d
sena <sena@mac.lan>
2026-05-05 16:43:56 -0400

    changed docker compose commands

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 # set environment variables
 . ~/workspace/glyTableMaker-backend/bashrc
 # start apache
-cd ~/workspace/glyTableMaker-backend/apache && docker-compose up -d
+cd ~/workspace/glyTableMaker-backend/apache && docker compose up -d
 # start postgres
-cd ~/workspace/glyTableMaker-backend/postgres && docker-compose up -d
+cd ~/workspace/glyTableMaker-backend/postgres && docker compose up -d
 # start backend application
-cd ~/workspace/glyTableMaker-backend && docker-compose up -d
+cd ~/workspace/glyTableMaker-backend && docker compose up -d
 # start frontend application
-cd ~/workspace/glyTableMaker-frontend && docker-compose up -d --build && docker images -qf dangling=true | xargs docker rmi
+cd ~/workspace/glyTableMaker-frontend && docker compose up -d --build && docker images -qf dangling=true | xargs docker rmi
